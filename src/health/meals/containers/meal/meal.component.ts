07d15a20c029028b99b9882c416d497afa465a2a
Hugo Noro <norogithub@gmail.com>
2019-01-09 14:02:50 +0000

    Component architecture and Reactive Forms

--- src/health/meals/containers/meal/meal.component.ts
+++ src/health/meals/containers/meal/meal.component.ts
@@ -1,19 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { Meal } from '../../../shared/services/meals/meals.service';
 
 @Component({
-  selector: 'app-meal',
-  styleUrls: ['./meal.component.scss'],
-  template: `
-    <div class="meal">
-      I am a meal!
-    </div>
-  `
+    selector: 'app-meal',
+    styleUrls: ['./meal.component.scss'],
+    template: `
+      <div class="meal">
+        <div class="meal__title">
+          <h1>
+            <img src="/assets/img/food.svg">
+            <span>Create meal</span>
+          </h1>
+        </div>
+        <app-meal-form
+                (create)="addMeal($event)">
+        </app-meal-form>
+      </div>
+    `
 })
-export class MealComponent implements OnInit {
+export class MealComponent {
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
+    addMeal(meal: Meal) {
+        console.log(meal);
+    }
 
 }
--- src/health/meals/meals.module.ts
+++ src/health/meals/meals.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { SharedModule } from '../shared/shared.module';
+import { MealFormComponent } from './components/meal-form/meal-form.component';
 import { MealsComponent } from './containers/meals/meals.component';
 import { MealsRoutingModule } from './meals-routing.module';
 import { MealComponent } from './containers/meal/meal.component';
@@ -10,7 +11,8 @@ import { MealComponent } from './containers/meal/meal.component';
 @NgModule({
     declarations: [
         MealsComponent,
-        MealComponent
+        MealComponent,
+        MealFormComponent
     ],
     imports: [
         CommonModule,
